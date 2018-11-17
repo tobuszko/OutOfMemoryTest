1c1936fa4e170394bbfca29f800dba86d05bf191
tobuszko <tomaszobuszko@gmail.com>
2018-11-17 16:59:27 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i==i;  i += 400){
+            long [][] tablica = new long[i][i];
+            System.out.println("Tablica o wymiarach: " + i + " na " + i);
+        }
+
+
+    }
+}
